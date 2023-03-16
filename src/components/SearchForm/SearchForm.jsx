9a6ab3cea01664fead2ae2d389f6031f105b7c61
R-Sona <126438245+R-Sona@users.noreply.github.com>
2023-03-16 17:20:32 +0530

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -27,21 +27,29 @@ const SearchForm = () => {
     
   
   return (
-    <div className='search-form'>
-    <div className='container'>
-      <div className='search-form-content'>
-        <form className='search-form' onSubmit={handleSubmit}  >
-        
-          <div className='search-form-elem flex flex-sb bg-white'>
-            <input type = "text" className='form-control' placeholder='The Lost World ...' ref={searchText}  />
-            <button type = "submit" className='flex flex-c' onClick={handleSubmit} >
-              <FaSearch className='text-purple' size = {32} />
-            </button>
-          </div>
-        </form>
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="The Lost World ..."
+                ref={searchText}
+              />
+              <button
+                type="submit"
+                className="search-btn"
+                onClick={handleSubmit}
+              >
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
