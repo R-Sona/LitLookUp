fc8dccb24ab159c5b796dcf01a8fcadf14fcf93d
R-Sona <126438245+R-Sona@users.noreply.github.com>
2023-03-24 13:36:16 +0530

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,3 +1,51 @@
+// import React, {useRef, useEffect} from 'react';
+// import {FaSearch} from "react-icons/fa";
+// import { useNavigate } from 'react-router-dom';
+// import { useGlobalContext } from '../../context';
+// import "./SearchForm.css";
+
+// const SearchForm = () => {
+//     const {setSearchTerm, setResultTitle}= useGlobalContext();
+//     const searchText = useRef('');
+//     const navigate = useNavigate();
+
+//     useEffect(() => searchText.current.focus(), []);
+//     const handleSubmit = (e) => {
+//       e.preventDefault();
+//       let tempSearchTerm= searchText.current.value.trim();
+//       if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
+//         setSearchTerm("the lost world");
+//         setResultTitle("Please Enter Something...");
+//       }else{
+//         setSearchTerm(searchText.current.value);
+
+
+//       }
+//       navigate("/book");
+//     }
+ 
+    
+  
+//   return (
+//     <div className="search-form">
+//   <form onSubmit={handleSubmit}>
+//     <div className="search-bar">
+//       <input
+//         type="text"
+//         placeholder="Search for products"
+//         ref={searchText}
+//       />
+//       <button type="submit" className="search-btn">
+//         <FaSearch className="text-purple" size={32} />
+//       </button>
+//     </div>
+//   </form>
+// </div>
+//   )
+// }
+
+// export default SearchForm
+
 import React, {useRef, useEffect} from 'react';
 import {FaSearch} from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
@@ -5,43 +53,40 @@ import { useGlobalContext } from '../../context';
 import "./SearchForm.css";
 
 const SearchForm = () => {
-    const {setSearchTerm, setResultTitle}= useGlobalContext();
-    const searchText = useRef('');
-    const navigate = useNavigate();
+  const {setSearchTerm, setResultTitle}= useGlobalContext();
+  const searchText = useRef('');
+  const navigate = useNavigate();
 
-    useEffect(() => searchText.current.focus(), []);
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      let tempSearchTerm= searchText.current.value.trim();
-      if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
-        setSearchTerm("the lost world");
-        setResultTitle("Please Enter Something...");
-      }else{
-        setSearchTerm(searchText.current.value);
+  useEffect(() => searchText.current.focus(), []);
 
-
-      }
-      navigate("/book");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempSearchTerm= searchText.current.value.trim();
+    if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
+      setSearchTerm("the lost world");
+      setResultTitle("Please Enter Something...");
+    }else{
+      setSearchTerm(searchText.current.value);
     }
- 
-    
-  
+    navigate("/book");
+  }
+
   return (
     <div className="search-form">
-  <form onSubmit={handleSubmit}>
-    <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for products"
-        ref={searchText}
-      />
-      <button type="submit" className="search-btn">
-        <FaSearch className="text-purple" size={32} />
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search for products"
+            ref={searchText}
+          />
+          <button type="submit" className="search-btn">
+            <FaSearch className="text-purple" size={32} />
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
-</div>
-  )
+  );
 }
 
-export default SearchForm
+export default SearchForm;
