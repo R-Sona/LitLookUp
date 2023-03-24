27f6bbcc800894c4beb92c403da8d8d9ff2ff7a2
R-Sona <126438245+R-Sona@users.noreply.github.com>
2023-03-24 14:15:56 +0530

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -46,47 +46,56 @@
 
 // export default SearchForm
 
-import React, {useRef, useEffect} from 'react';
-import {FaSearch} from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
-import { useGlobalContext } from '../../context';
+import React, { useRef, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../context";
 import "./SearchForm.css";
 
 const SearchForm = () => {
-  const {setSearchTerm, setResultTitle}= useGlobalContext();
-  const searchText = useRef('');
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
+  const searchText = useRef("");
   const navigate = useNavigate();
 
   useEffect(() => searchText.current.focus(), []);
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    let tempSearchTerm= searchText.current.value.trim();
-    if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
+    let tempSearchTerm = searchText.current.value.trim();
+    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length === 0) {
       setSearchTerm("the secret");
       setResultTitle("Please Enter Something...");
-    }else{
+    } else {
       setSearchTerm(searchText.current.value);
     }
     navigate("/book");
-  }
+  };
 
   return (
     <div className="search-form">
-      <form onSubmit={handleSubmit}>
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search for products"
-            ref={searchText}
-          />
-          <button type="submit" className="search-btn">
-            <FaSearch className="text-purple" size={32} />
-          </button>
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="The Secret..."
+                ref={searchText}
+              />
+              <button
+                type="submit"
+                className="search-btn"
+                onClick={handleSubmit}
+              >
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
-}
+};
 
 export default SearchForm;
+
